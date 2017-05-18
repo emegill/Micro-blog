20ed1b13d3cac525b7b7dd15690dd7d1de2136f1
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-18 17:20:08 -0400

    Adds sign up and sign in functionality.

--- app.rb
+++ app.rb
@@ -5,3 +5,28 @@ set :database, 'sqlite3:app.sqlite3'
 set :sessions, true
 
 require './models'
+
+get '/' do
+    erb :home
+end
+
+post '/sign_in' do
+    user = User.where(username: params[:username]).first
+    if user.password == params[:password]
+        session[:user_id] = user.id
+        redirect "/account/#{user.id}"
+    else
+        redirect '/'
+    end
+end
+
+post '/create_user' do
+    user = User.create(params[:user])
+    session[:user_id] = user.id
+    redirect "/account/#{user.id}"
+end
+
+get '/account/:id' do
+    user = User.find(params[:id])
+    erb :account
+end
