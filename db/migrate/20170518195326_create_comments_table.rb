4a3b838e599d44953d1e6046af0bd2c947aa2775
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-18 15:55:58 -0400

    Sets up relational database with users, blog and comments.

---
+++ db/migrate/20170518195326_create_comments_table.rb
@@ -0,0 +1,9 @@
+class CreateCommentsTable < ActiveRecord::Migration[5.1]
+  def change
+      create_table :comments do |t|
+          t.integer :user_id
+          t.integer :blog_id
+          t.string :body
+      end
+  end
+end
